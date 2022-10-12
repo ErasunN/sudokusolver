1240ae0ba2e21e7b7b2e9165e775804eb3c231bd
Nicolas <nicoerasun98@gmail.com>
2022-10-11 21:59:38 -0300

    Updates

---
+++ precache-manifest.f5628a3a9d4d37b83df9392c80decf3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "adc57f0eebafcec48c23",
+    "url": "/sudokusolver/static/css/main.8d811f39.chunk.css"
+  },
+  {
+    "revision": "adc57f0eebafcec48c23",
+    "url": "/sudokusolver/static/js/main.adc57f0e.chunk.js"
+  },
+  {
+    "revision": "944747089252c95a258e",
+    "url": "/sudokusolver/static/js/1.94474708.chunk.js"
+  },
+  {
+    "revision": "90eca83e3b6d51a0c3cc",
+    "url": "/sudokusolver/static/js/2.90eca83e.chunk.js"
+  },
+  {
+    "revision": "b8e164d3c280f131c046",
+    "url": "/sudokusolver/static/js/runtime~main.b8e164d3.js"
+  },
+  {
+    "revision": "be0653c5c521921219062382fe868fd1",
+    "url": "/sudokusolver/index.html"
+  }
+];
